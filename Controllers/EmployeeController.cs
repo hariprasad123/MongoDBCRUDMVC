510fff30c574ee1aa67e01243257e26d814490b6
hariprasad123 <hariprasad.chintala@gmail.com>
2018-12-17 15:35:02 +0530

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCTest.Models;
+using System.Configuration;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Bson;
+
+namespace MVCTest.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //[NonAction]
+        //public List<Employee> GetEmployeeList()
+        //{
+        //    return new List<Employee>
+        //    {
+        //        new Employee
+        //        {
+        //            id = 1,
+        //            name = "Hari"
+        //        },
+        //        new Employee
+        //        {
+        //            id = 2,
+        //            name = "CS"
+        //        },
+        //        new Employee
+        //        {
+        //            id = 3,
+        //            name = "SS"
+        //        }
+        //    };
+        //}
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return RedirectToAction("EmpList");
+        }
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Insert(Employee emp)
+        {
+            
+            if (ModelState.IsValid)
+            {   
+                string constr = ConfigurationManager.AppSettings["connectionString"];
+                var client = new MongoClient(constr);
+                var db = client.GetDatabase("CarDatabase");
+                var collection = db.GetCollection<Employee>("EmployeeDetails");
+                //var collection = db.GetCollection<BsonDocument>("EmployeeDetails");
+                collection.InsertOneAsync(emp);
+                //collection.InsertOneAsync<Employee>(emp);
+                return RedirectToAction("EmpList");
+            }
+            return View();
+        }
+
+        public ActionResult EmpList()
+        {
+            string constr = ConfigurationManager.AppSettings["connectionString"];
+            var Client = new MongoClient(constr);
+            var db = Client.GetDatabase("CarDatabase");
+            var collection = db.GetCollection<Employee>("EmployeeDetails").Find(new BsonDocument()).ToList();
+
+            return View(collection);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            if (ModelState.IsValid)
+            {
+                var empObjectId = Builders<Employee>.Filter.Eq(p => p.Id, new ObjectId(id));
+                string constr = ConfigurationManager.AppSettings["connectionString"];
+                var Client = new MongoClient(constr);
+                var DB = Client.GetDatabase("CarDatabase");
+                var collection = DB.GetCollection<Employee>("EmployeeDetails");
+                //var deleteRecord = collection.DeleteOne(Builders<Employee>.Filter.Eq(r => r.Id, new ObjectId(id)));
+                var deleteRecord = collection.DeleteOne(empObjectId);
+                return RedirectToAction("EmpList");
+            }
+            return View();
+
+        }
+
+        public ActionResult Edit(Employee Empdet)
+        {
+            if (ModelState.IsValid)
+            {
+                string constr = ConfigurationManager.AppSettings["connectionString"];
+                var Client = new MongoClient(constr);
+                var Db = Client.GetDatabase("CarDatabase");
+                var collection = Db.GetCollection<Employee>("EmployeeDetails");
+
+                var update = collection.FindOneAndUpdateAsync(Builders<Employee>.Filter.Eq("Id", Empdet.Id), Builders<Employee>.Update.Set("Name", Empdet.Name).Set("Department", Empdet.Department).Set("Address", Empdet.Address).Set("City", Empdet.City).Set("Country", Empdet.Country));
+
+                return RedirectToAction("EmpList");
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MVCTest.Models
+{
+    public class Employee
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        //public object _id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
